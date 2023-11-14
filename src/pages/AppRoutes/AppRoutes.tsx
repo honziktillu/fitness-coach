10178ad699b576048f0d433a8e1a13ab8dc7853b
honziktillu <till@spsmb.cz>
2023-11-14 08:54:57 +0100

    added dashboard

--- src/pages/AppRoutes/AppRoutes.tsx
+++ src/pages/AppRoutes/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { MainPage } from "../MainPage/MainPage";
+import Dashboard from "../Dashboard/Dashboard";
 
 export function AppRoutes() {
     return (
         <HashRouter>
             <Routes>
                 <Route path="/" element={<MainPage />} />
-                <Route />
+                <Route path="/dashboard" element={<Dashboard />}/>
                 <Route />
             </Routes>
         </HashRouter>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,12 @@
-
+import { Link } from "react-router-dom"
 
 export function MainPage() {
     return (
         <>
             <h1>Hello world</h1>
+            <Link to={"/dashboard"}>
+                <p>Dashboard</p>
+            </Link>
         </>
     )
 }
